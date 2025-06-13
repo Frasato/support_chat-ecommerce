cdd984e4152b29195abe7fe85e9fd5731530ab6c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-13 09:36:37 -0300

    :wrench: chore: Configure Routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {path: '', element: <Home />}
+        ]
+    }
+]);
