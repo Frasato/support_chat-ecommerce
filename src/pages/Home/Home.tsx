90cd1ccea566b9d24c35e950d24b09f6ef7f6461
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-13 10:01:51 -0300

    :sparkles: feat: Add opens chats and chat on home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import Chat from "../../components/chat/Chat";
+import OpenChats from "../../components/openChats/OpenChats";
+
 const Home = () => {
     return(
-        <>
-        </>
+        <section>
+            <OpenChats />
+            <Chat />
+        </section>
     );
 }
 
