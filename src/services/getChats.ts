8276f580cdf7db5464031f40e3e91ac69e688d85
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-16 12:45:09 -0300

    :sparkles: feat: List all chats and get history for chats

--- src/services/getChats.ts
+++ src/services/getChats.ts
@@ -1,14 +1,37 @@
-import type { ChatsType } from "../types/ChatsType";
+import type { ChatDto, ResponseListChatsDto } from "../types/ChatsType";
 
-export async function getChats(): Promise<ChatsType[]> {
-    const apiUrl: string = import.meta.env.VITE_API_URL;
-    
-    try{
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        return data.chats;
-    }catch(error){
-        console.error(error);
-        return [];
+const apiUrl = import.meta.env.VITE_API_URL || '';
+const token: string = localStorage.getItem('token') || '';
+
+export async function listChats(): Promise<ResponseListChatsDto>{
+    const response = await fetch(`${apiUrl}/chat`, {
+        method: 'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'Authorization':'Bearer '+token
+        },
+    });
+
+    if(!response.ok){
+        throw new Error('Failed to list chats');
     }
+
+    return response.json();
+}
+
+export async function historyChat(chatId: string): Promise<ChatDto[]>{
+    const response = await fetch(`${apiUrl}/chat/${chatId}/messages`, {
+        method: 'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'Authorization':'Bearer '+token
+        }
+    });
+
+    if(!response.ok){
+        throw new Error('Failed to get history chat');
+    }
+
+    const data: ChatDto[] = await response.json();
+    return data;
 }
