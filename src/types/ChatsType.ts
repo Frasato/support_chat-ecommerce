532ee1951868adcca4aa8f4c9514dc2e2f2651a8
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-16 12:44:37 -0300

    :sparkles: feat: Create Chat Interfaces

--- src/types/ChatsType.ts
+++ src/types/ChatsType.ts
@@ -1,6 +1,18 @@
-export type ChatsType = {
-    chatId: string,
-    userId: string,
-    active: boolean,
-    createdAt: string
+export interface Chat{
+    chatId: string;
+    userId: string;
+    active: boolean;
+}
+
+export interface ChatDto{
+    chatId: string;
+    userId: string | null;
+    message: string;
+    senderType: 'CLIENT' | 'ADMIN' | 'SYSTEM';
+    active: boolean;
+    timestamp: string;
+}
+
+export interface ResponseListChatsDto{
+    chats: Chat[];
 }
