056e25ce882ccc90b68f611b45d4e27abd6d2ff8
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-16 12:44:23 -0300

    :sparkles: feat: Add Login o routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
-            {path: '', element: <Home />}
+            {path: '', element: <Home />},
+            {path: 'login', element: <Login />}
         ]
     }
 ]);
