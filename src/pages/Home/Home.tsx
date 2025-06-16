f887848acd9f29a42625e2eb87d8695db54cc42f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-16 12:42:59 -0300

    Create Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,47 @@
-import Chat from "../../components/chat/Chat";
-import OpenChats from "../../components/openChats/OpenChats";
+import { useCallback, useEffect, useState } from "react";
+import type { Chat } from "../../types/ChatsType";
+import { listChats } from "../../services/getChats";
+import ChatList from "../../components/openChats/ChatList";
+import ChatWindow from "../../components/chat/ChatWindow";
 
-const Home = () => {
-    return(
-        <section>
-            <OpenChats />
-            <Chat />
-        </section>
+const Home: React.FC = () => {
+  
+  const [chats, setChats] = useState<Chat[]>([]);
+  const [selectedChatId, setSelectedChatId] = useState<string | null>('');
+
+  const loadsChats = useCallback(async ()=>{
+    try {
+      
+      const data = await listChats();
+      setChats(data.chats);
+
+    }catch(error){
+      setChats([]);
+      throw new Error(`Error on load chats: ${error}`);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadsChats();
+    const interval = setInterval(loadsChats, 30000);
+    return () => clearInterval(interval);
+  }, [loadsChats])
+  
+  const handleChatClose = () =>{
+    setSelectedChatId(null);
+    loadsChats();
+  }
+
+  return (
+    <div style={{ display: 'flex', height: '100vh', fontFamily: 'Arial, sans-serif' }}>
+      <ChatList
+        chats={chats}
+        selectedChatId={selectedChatId}
+        onSelect={setSelectedChatId}
+      />
+      <ChatWindow chatId={selectedChatId} onChatClosed={handleChatClose}/>
+    </div>
     );
-}
-
-export default Home;
+  };
+  
+  export default Home;
